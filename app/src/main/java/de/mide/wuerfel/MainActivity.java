f03bb9f675cd94d6135d4c5d37c257692578c639
MDecker-MobileComputing <mide_42b@yahoo.de>
2021-09-12 16:09:14 +0200

    App inkl. Fehler vorerst fertig.

--- app/src/main/java/de/mide/wuerfel/MainActivity.java
+++ app/src/main/java/de/mide/wuerfel/MainActivity.java
@@ -1,16 +1,64 @@
 package de.mide.wuerfel;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
 
+/**
+ * Fehlersuchaufgabe: App als Ersatz für einen echten Würfel.
+ * <br><br>
+ *
+ * This project is licensed under the terms of the BSD 3-Clause License.
+ */
+public class MainActivity extends Activity implements OnClickListener {
+
+    /**
+     * Zufallsgenerator; die Methode {@code nextInt(obergrenze)} diese Objects erzeugt eine
+     * Zufallszahl zwischen Null und {@code obergrenze-1} (jeweils einschließlich).
+     */
+    final private Random _random = new Random();
+
+    /** TextView-Element zur Anzeige der gewürfelten Zahl. */
+    private TextView _zahlTextView = null;
+
+    /** Button um eine neue Zahl von 1-6 zu würfen. */
+    private Button _wuerfelButton1 = null;
+
+    /**
+     * Lifecycle-Methode: Lädt Layout-Datei, füllt Member-Variablen für UI-Elemente,
+     * setzt Event-Handler-Objekt für Button.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+        _zahlTextView   = findViewById(R.id.zahlTextView);
+        _wuerfelButton1 = findViewById(R.id.wuerfelButton_1);
+
         setContentView(R.layout.activity_main);
+
+        _wuerfelButton1.setOnClickListener(this);
     }
-    
+
+    /**
+     * Event-Handler-Methode für Button.
+     *
+     * @param view  UI-Element, welches das Event ausgelöst hat.
+     */
+    @Override
+    public void onClick(View view) {
+
+        // Zufallszahl zwischen 1..6 erzeugen (normaler Würfel)
+        int zufallszahl = _random.nextInt(6) + 1;
+
+        _zahlTextView.setText(zufallszahl + "");
+    }
+
 }
